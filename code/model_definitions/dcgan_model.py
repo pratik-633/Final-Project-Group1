dce6d470c5f2388b278bd7f59fdd661403913631
pratik-633 <pratiks@gwmail.gwu.edu>
2026-04-10 17:18:58 -0400

    imported DCGAN model definition

---
+++ code/model_definitions/dcgan_model.py
@@ -0,0 +1,87 @@
+import torch
+import torch.nn as nn
+
+
+class Generator_DCGAN(nn.Module):
+    def __init__(self, latent_dim, channels, feature_maps=64):
+        super(Generator_DCGAN, self).__init__()
+
+        self.network = nn.Sequential(
+            # Input: (batch, latent_dim, 1, 1)
+
+            # 1x1 -> 4x4
+            nn.ConvTranspose2d(latent_dim, feature_maps * 8, kernel_size=4, stride=1, padding=0, bias=False),
+            nn.BatchNorm2d(feature_maps * 8),
+            nn.ReLU(True),
+
+            # 4x4 -> 8x8
+            nn.ConvTranspose2d(feature_maps * 8, feature_maps * 4, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(feature_maps * 4),
+            nn.ReLU(True),
+
+            # 8x8 -> 16x16
+            nn.ConvTranspose2d(feature_maps * 4, feature_maps * 2, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(feature_maps * 2),
+            nn.ReLU(True),
+
+            # 16x16 -> 32x32
+            nn.ConvTranspose2d(feature_maps * 2, feature_maps, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(feature_maps),
+            nn.ReLU(True),
+
+            # 32x32 -> 64x64
+            nn.ConvTranspose2d(feature_maps, channels, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.Tanh()
+        )
+
+    def forward(self, x):
+        return self.network(x)
+
+
+class Discriminator_DCGAN(nn.Module):
+    def __init__(self, channels, feature_maps=64):
+        super(Discriminator_DCGAN, self).__init__()
+
+        self.network = nn.Sequential(
+            # Input: (batch, channels, 64, 64)
+
+            # 64x64 -> 32x32
+            nn.Conv2d(channels, feature_maps, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.LeakyReLU(0.2, inplace=True),
+
+            # 32x32 -> 16x16
+            nn.Conv2d(feature_maps, feature_maps * 2, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(feature_maps * 2),
+            nn.LeakyReLU(0.2, inplace=True),
+
+            # 16x16 -> 8x8
+            nn.Conv2d(feature_maps * 2, feature_maps * 4, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(feature_maps * 4),
+            nn.LeakyReLU(0.2, inplace=True),
+
+            # 8x8 -> 4x4
+            nn.Conv2d(feature_maps * 4, feature_maps * 8, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(feature_maps * 8),
+            nn.LeakyReLU(0.2, inplace=True),
+
+            # 4x4 -> 1x1
+            nn.Conv2d(feature_maps * 8, 1, kernel_size=4, stride=1, padding=0, bias=False),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        return self.network(x).view(-1, 1)
+
+
+class DCGAN(nn.Module):
+    def __init__(self, latent_dim, channels, feature_maps=64):
+        super(DCGAN, self).__init__()
+        self.generator = Generator_DCGAN(
+            latent_dim=latent_dim,
+            channels=channels,
+            feature_maps=feature_maps
+        )
+        self.discriminator = Discriminator_DCGAN(
+            channels=channels,
+            feature_maps=feature_maps
+        )
